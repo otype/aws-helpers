b80f304ad7323ed0037d8c27bbb8912840778577
Hans-Gunther Schmidt <hans@otype.de>
2017-12-05 13:11:40 +0100

    Add setup.py to make this pip-installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='aws-helpers',
+    description='Set of AWS helper scripts',
+    url='https://github.com/otype/aws-helpers',
+    author='Hans-Gunther Schmidt',
+    author_email='hans@otype.de',
+    version='0.1',
+    scripts=['aws-instances']
+ )
