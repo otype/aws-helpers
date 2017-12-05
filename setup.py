4c9b81466ebbba1f61e8f3ba3e2ff2ed2ec932d9
Hans-Gunther Schmidt <hans@otype.de>
2017-12-05 13:46:04 +0100

    Add dependencies for github links

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     author='Hans-Gunther Schmidt',
     author_email='hans@otype.de',
     version='0.1',
+    packages=find_packages(),
+    install_requires=['awscli>=2.48.0', 'boto>=1.14.3'],
+    dependency_links=[
+        "git+ssh://git@github.com/makethunder/awsudo.git@egg=awsudo"
+    ],
     scripts=['src/aws-instances', 'src/aws-instances.py']
  )
